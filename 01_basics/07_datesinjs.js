b18969af3c9dbf2a62522c284c89a300aaa83cc9
tanishjain09 <tanish.jain.0905@gmail.com>
2025-06-19 08:40:35 +0530

    dates in js

--- 01_basics/07_datesinjs.js
+++ 01_basics/07_datesinjs.js
@@ -11,20 +11,20 @@ let myDate =new Date()
 // let myCreatedDate = new Date(2023,0,23)
 let myCreatedDate = new Date("01-14-2023")
 
-// console.log(myCreatedDate);
+// console.log(myCreatedDate); 
 // console.log(myCreatedDate.toLocaleString());
 
 let myTimeStamp = Date.now()
 
 // console.log(myTimeStamp);
-// console.log(myCreatedDate.getTime());
+// console.log(myCreaṇtedDate.getTime());
 // console.log(Math.floor(Date.now()/1000));
 
 let newDate = new Date()
-console.log(newDate);
-console.log(newDate.getDate());
-console.log(newDate.getDay());
-console.log(newDate.getMonth()+1);
+// console.log(newDate);
+// console.log(newDate.getDate());
+// console.log(newDate.getDay());
+// console.log(newDate.getMonth()+1);
 
 newDate.toLocaleString('default',{
     weekday: "long",  
