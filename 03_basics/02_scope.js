b0f5f30fd6d31274d8977b58d5a9d63e8e895c5e
tanishjain09 <tanish.jain.0905@gmail.com>
2025-06-27 13:57:29 +0530

    adding scope file

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -3,11 +3,38 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20
-    console.log("INNER: ",a);
+    // console.log("INNER: ",a);
     
 }
 
 
-console.log("OUTER: ",a);
+// console.log("OUTER: ",a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "Tanish"
+
+    function two(){
+        const wesite = "youtube"
+        console.log(username)
+    }
+    // console.log(wesite); //can't use website outsite two() function
+    two()
+}
+
+// one()
+
+// ++++++++++++++++ interesting ++++++++++++++++++
+
+function addOne(num){
+    return num + 1
+}
+
+addOne(4)
+
+const addTwo = function(num){
+    return num + 2
+}
+
+addTwo(5)
