bd91a5cf3afa5774bc7821aac9ac3fa7bd0ca889
tanishjain09 <tanish.jain.0905@gmail.com>
2025-07-17 08:54:52 +0530

    oops in javascript

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -1,7 +1,7 @@
 const user = {
     username: "Tanish",
     loginCount: 8,
-    signedIN: true,
+    signedIn: true,
 
     getUserDetails: function(){
         //console.log("Got user details from database");
@@ -15,6 +15,11 @@ const user = {
 // const promiseOne = new Promise()
 // const date = new Date()
 
+
+// console.log(this);
+//the thing is when we console log the this in global space in node.js it return {} as it has nothing in context in global scope
+//while if we print this is same in brower in global scope it gives window because brower so many other thing also like dom api,fetch() and many more.
+
 function User(username, loginCount, isLoggedIn){
     this.username = username;
     this.loginCount = loginCount;
@@ -22,10 +27,14 @@ function User(username, loginCount, isLoggedIn){
     this.greeting = function(){
         console.log(`Welcome ${this.username}`);
     }
-    return this
+    // return this
 }
 
 const userOne = new User("tanish", 12, true)
-const userTwo = new User("baigan", 48, true)
-console.log(userOne.constructor);
+const userTwo = User("baigan", 48, true)
+console.log(userOne.constructor); //reference to own
 // console.log(userTwo);
+
+
+console.log(userOne instanceof User);
+console.log(userTwo instanceof User);
